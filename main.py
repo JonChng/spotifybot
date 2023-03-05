56b89be1295d2740196fdce77652b9217e16cf85
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-05 22:49:08 +0800

    new commit

--- main.py
+++ main.py
@@ -3,26 +3,28 @@ import requests
 import dotenv
 import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import  SpotifyOAuth
 
 dotenv.load_dotenv()
-secret = os.environ["SPOTIFY-SERCRET"]
+secret = os.environ["SPOTIFY-SECRET"]
 id = os.environ["SPOTIFY-ID"]
 
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(client_id=id, client_secret=secret, redirect_uri="http://localhost:8888/callback", scope="playlist-modify-private")
+)
 
 
-
-date = input("What year do you want to travel to? Input date in format of (YYYY-MM-DD): ")
-
-url = f"https://www.billboard.com/charts/hot-100/{date}"
-
-response = requests.get("https://www.billboard.com/charts/hot-100/")
-soup = BeautifulSoup(response.text,'html.parser')
-
-
-responses = soup.find_all("h3", class_='a-no-trucate')
-songs = [i.getText().strip() for i in responses]
-print(songs)
+# date = input("What year do you want to travel to? Input date in format of (YYYY-MM-DD): ")
+#
+# url = f"https://www.billboard.com/charts/hot-100/{date}"
+#
+# response = requests.get("https://www.billboard.com/charts/hot-100/")
+# soup = BeautifulSoup(response.text,'html.parser')
+#
+#
+# responses = soup.find_all("h3", class_='a-no-trucate')
+# songs = [i.getText().strip() for i in responses]
+# print(songs)
 
 
 
