b7d67e2e699d28d49b6d9b198ac3b194b97f524e
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-06 21:32:31 +0800

    new commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ secret = os.environ["SPOTIFY-SECRET"]
 id = os.environ["SPOTIFY-ID"]
 
 sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(client_id=id, client_secret=secret, redirect_uri="http://localhost:8888/callback", scope="playlist-modify-private")
+    auth_manager=SpotifyOAuth(client_id=id, client_secret=secret, redirect_uri="http://localhost:8888/callback", scope="playlist-modify-public")
 )
 user_id = sp.current_user()["id"]
 
@@ -28,18 +28,22 @@ responses = soup.find_all("h3", class_='a-no-trucate')
 songs = [i.getText().strip() for i in responses]
 year = date[0:4]
 start = str(int(year) - 3)
-range = f"{start}-{year}"
+range_ = f"{start}-{year}"
 
 print(songs)
 
 
 for song in songs:
-    result = sp.search(q=f"track:{song} year:{range}", type="track")
+    result = sp.search(q=f"track:{song} year:{range_}", type="track")
     try:
-        song_list.append(result['tracks']['items'][0]['album']['artists'][0]['id'])
+        song_list.append(result['tracks']['items'][0]['uri'])
     except IndexError:
         print("Song does not exist. " + song)
 
+playlist = sp.user_playlist_create(user_id, name=f"{date} Billboard 100", description="Top 100 Songs", public=True)
+playlist_id = playlist['id']
 
+sp.playlist_add_items(playlist_id=playlist_id, items=song_list, position=0)
 
+print("Playlist created.")
 
